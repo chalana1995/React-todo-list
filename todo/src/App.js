1baed9093c4926fa67b3a552d13c4e32fad9eeb2
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-06-20 13:18:07 +0530

    finish react todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
-const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
+  function addtask() {
+    setTasks([...tasks, newTask]);
+  }
 
- function addtask(){
-   alert(newTask);
- }
+  function deleteTask(index) {
+    var duplicateArray = [...tasks];
+    duplicateArray.splice(index, 1);
+    setTasks(duplicateArray);
+  }
+
+  const taskList = tasks.map((object, index) => {
+    return (
+      <div className='row justify-content-center'>
+        <h1 className='col-md-6 text-left'>{index + 1}. {object}</h1>
+        <button onClick={() => { deleteTask(index) }} className='btn btn-danger col-md-1 m-1'>DELETE</button>
+      </div>
+
+    )
+  })
 
 
   return (
@@ -19,12 +35,14 @@ const [newTask, setNewTask] = useState('');
 
       <div className='row justify-content-center'>
         <div className='col-md-6'>
-          <input type='text' placeholder='taskname' className='form-control' 
-          value={newTask} onChange={(e) => {setNewTask(e.target.value)}}
+          <input type='text' placeholder='taskname' className='form-control'
+            value={newTask} onChange={(e) => { setNewTask(e.target.value) }}
           />
         </div>
         <button onClick={addtask} className='btn btn-success col-md-1'>ADD TASK</button>
       </div>
+
+      {taskList}
     </div>
   );
 }
